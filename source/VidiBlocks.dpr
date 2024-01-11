81691dc9c1c73ee865deaed26f746082847c8c6e
david berneda <davidberneda@gmail.com>
2024-01-11 22:09:31 +0100

    no message

--- source/VidiBlocks.dpr
+++ source/VidiBlocks.dpr
@@ -1,26 +1,27 @@
-// @davidberneda June 2023
+// @davidberneda Jan 2024
 program VidiBlocks;
 
 uses
   System.StartUpCopy,
   FMX.Forms,
   FMX.Skia,
-  Unit_Blocks in 'Unit_Blocks.pas' {FormMain},
-  Unit_Settings in 'Unit_Settings.pas' {FormSettings},
-  Unit_Texts in 'Unit_Texts.pas',
-  Vidi_FMX in '..\..\..\SharedUnits\Vidi_FMX.pas',
-  Unit_Animate in '..\..\Unit_Animate.pas',
-  Unit_Game in '..\..\Unit_Game.pas',
-  Vidi_Graphics in '..\..\Vidi_Graphics.pas',
-  Vidi_Game in 'Vidi_Game.pas',
-  Vidi_Canvas in '..\..\Vidi_Canvas.pas',
-  Unit_Sounds in '..\..\Unit_Sounds.pas',
-  Unit_BestScores in 'Unit_BestScores.pas' {FormScores};
+  Unit_Blocks in 'fmx\Unit_Blocks.pas' {FormMain},
+  Unit_Settings in 'fmx\Unit_Settings.pas' {FormSettings},
+  Unit_Texts in 'fmx\Unit_Texts.pas',
+  Vidi_FMX in 'fmx\Vidi_FMX.pas',
+  Unit_Animate in 'common\Unit_Animate.pas',
+  Unit_Game in 'common\Unit_Game.pas',
+  Vidi_Graphics in 'common\Vidi_Graphics.pas',
+  Vidi_Game in 'fmx\Vidi_Game.pas',
+  Vidi_Canvas in 'common\Vidi_Canvas.pas',
+  Unit_Sounds in 'common\Unit_Sounds.pas',
+  Unit_BestScores in 'fmx\Unit_BestScores.pas' {FormScores};
 
 {$R *.res}
 
 begin
-  GlobalUseSkia := True;
+  GlobalUseSkia := False;
+
   {$IFOPT D+}
   ReportMemoryLeaksOnShutdown:=True;
   {$ENDIF}
